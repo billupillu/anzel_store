208ac4b05d655e2349c03bae8f2b2d5b3c0c27ee
Sumeet Masih <billu.pillu123@gmail.com>
2016-03-21 12:46:18 +0530

    s3 ki image get working test 1

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,89 +1,3 @@
-# # Configure Spree Preferences
-# #
-# # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-# #       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-# #
-# # Note: If a preference is set here it will be stored within the cache & database upon initialization.
-# #       Just removing an entry from this initializer will not make the preference value go away.
-# #       Instead you must either set a new value or remove entry, clear cache, and remove database entry.
-# #
-# # In order to initialize a setting do:
-# # config.setting_name = 'new value'
-# Spree.config do |config|
-#   # Example:
-#   # Uncomment to stop tracking inventory levels in the application
-#   # config.track_inventory_levels = false
-#   attachment_config = {
-
-#   s3_credentials: {
-#     access_key_id:     "AKIAJNKSQPCJ6QPMPAUQ",
-#     secret_access_key: "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",
-#     bucket:            "anzelstore"
-#   },
-
-#   storage:        :s3,
-#   s3_headers:     { "Cache-Control" => "max-age=31557600" },
-#   s3_protocol:    "https",
-#   bucket:         "anzelstore",
-#   url:            ":s3_domain_url",
-
-#   styles: {
-#       mini:     "48x48>",
-#       small:    "100x100>",
-#       product:  "240x240>",
-#       large:    "600x600>"
-#   },
-
-#   path:           "/:class/:id/:style/:basename.:extension",
-#   default_url:    "/:class/:id/:style/:basename.:extension",
-#   default_style:  "product"
-# }
-
-# attachment_config.each do |key, value|
-#   Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-# end
-# end
-
-# Spree.user_class = "Spree::User"
-
-
-
-
-#--------------------------------------
-# Spree.config do |config|
-#    # Example:
-#    # Uncomment to override the default site name.
-#   #  config.site_name = "Gazella Running Costumes"
-#   #  config.logo = "store/rungazella.png"
-
-#   #S3 configuration
-#   if Rails.env.production? then
-#        #production. Store images on S3.
-#        # development will default to local storage
-#       attachment_config = {
-#       s3_credentials: {
-#         access_key_id: "AKIAJNKSQPCJ6QPMPAUQ",
-#         secret_access_key: "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",
-#         bucket: "anzelstore",
-#       },
-
-
-#       storage:        :s3,
-#       s3_headers:     { "Cache-Control" => "max-age=31557600" },
-#       s3_protocol:    "https",
-#       bucket:         "anzelstore",
-
-      
-#       default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
-#       default_style: "product",
-#       }
-
-#       attachment_config.each do |key, value|
-#            Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-#       end
-#   end
-# Spree.user_class = "Spree::User"
-# end
 
 #--------------------------------------------
 Spree.config do |config|
@@ -97,6 +11,7 @@ Spree.config do |config|
   s3_headers:  {"Cache-Control"=>"max-age=31557600"},
   s3_protocol: "https",
   bucket:      ENV['S3_BUCKET_NAME'],
+  s3_host_name: "s3-us-west-2.amazonaws.com",
   url:         "url",
   styles:      {
                  mini:   "48x48>",
@@ -113,11 +28,10 @@ Spree.config do |config|
   end
 end
 
+# https://s3-us-west-2.amazonaws.com/anzelstore/spree/spree/images/10/large/Abstract_1_on_canvas__28x_31_inches__acrylic_on_canvas__11538_-_Copy.JPG
+# https://s3.amazonaws.com/anzelstore/spree/spree/images/10/mini/Abstract_1_on_canvas__28x_31_inches__acrylic_on_canvas__11538_-_Copy.JPG?1458365970
 
-
-
-
-
+    
 
 
 
